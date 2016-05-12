4f2f8933a66267d22d5b192855a07b932d2d1a90
Jorge Rodriguez <jorge.rodriguez08@icloud.com>
2016-05-12 01:19:14 -0500

    Second Commit
    Add Main Page Look and Feel

--- client/views/contact/contact.js
+++ client/views/contact/contact.js
@@ -3,7 +3,7 @@ Template.contact.onRendered(function() {
   this.autorun(function() {
     const baseURL = Meteor.absoluteUrl().slice(0, -1);
     setMeta({
-      title: 'Contacto',
+      title: 'Contact',
     });
   });
 });
